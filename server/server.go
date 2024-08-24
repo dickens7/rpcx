24652e0836f7ff2dac0d9ce7aaa92af401cf100c
lixiangyang <lixiangyang@dragonest.com>
2024-08-24 10:54:54 +0800

    feat: 异步send

--- server/server.go
+++ server/server.go
@@ -360,35 +360,37 @@ func (s *Server) sendResponse(ctx *share.Context, conn net.Conn, err error, req,
 		res.SetCompressType(req.CompressType())
 	}
 
-	s.Plugins.DoPreWriteResponse(ctx, req, res, err)
+	go func() {
+		s.Plugins.DoPreWriteResponse(ctx, req, res, err)
 
-	data := res.EncodeSlicePointer()
-	if s.AsyncWrite {
-		if s.pool != nil {
-			s.pool.Submit(func() {
-				if s.writeTimeout != 0 {
-					conn.SetWriteDeadline(time.Now().Add(s.writeTimeout))
-				}
-				conn.Write(*data)
-				protocol.PutData(data)
-			})
+		data := res.EncodeSlicePointer()
+		if s.AsyncWrite {
+			if s.pool != nil {
+				s.pool.Submit(func() {
+					if s.writeTimeout != 0 {
+						conn.SetWriteDeadline(time.Now().Add(s.writeTimeout))
+					}
+					conn.Write(*data)
+					protocol.PutData(data)
+				})
+			} else {
+				go func() {
+					if s.writeTimeout != 0 {
+						conn.SetWriteDeadline(time.Now().Add(s.writeTimeout))
+					}
+					conn.Write(*data)
+					protocol.PutData(data)
+				}()
+			}
 		} else {
-			go func() {
-				if s.writeTimeout != 0 {
-					conn.SetWriteDeadline(time.Now().Add(s.writeTimeout))
-				}
-				conn.Write(*data)
-				protocol.PutData(data)
-			}()
+			if s.writeTimeout != 0 {
+				conn.SetWriteDeadline(time.Now().Add(s.writeTimeout))
+			}
+			conn.Write(*data)
+			protocol.PutData(data)
 		}
-	} else {
-		if s.writeTimeout != 0 {
-			conn.SetWriteDeadline(time.Now().Add(s.writeTimeout))
-		}
-		conn.Write(*data)
-		protocol.PutData(data)
-	}
-	s.Plugins.DoPostWriteResponse(ctx, req, res, err)
+		s.Plugins.DoPostWriteResponse(ctx, req, res, err)
+	}()
 }
 
 func (s *Server) serveConn(conn net.Conn) {
