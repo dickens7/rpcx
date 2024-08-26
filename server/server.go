7ef124fd92c56b08da1c2f713d6c225792b3a7b8
lixiangyang <lixiangyang@dragonest.com>
2024-08-26 20:21:56 +0800

    feat: 修改为同步响应

--- server/server.go
+++ server/server.go
@@ -360,27 +360,25 @@ func (s *Server) sendResponse(ctx *share.Context, conn net.Conn, err error, req,
 		res.SetCompressType(req.CompressType())
 	}
 
-	go func() {
-		s.Plugins.DoPreWriteResponse(ctx, req, res, err)
+	s.Plugins.DoPreWriteResponse(ctx, req, res, err)
 
-		data := res.EncodeSlicePointer()
-		if s.AsyncWrite {
-			go func() {
-				if s.writeTimeout != 0 {
-					conn.SetWriteDeadline(time.Now().Add(s.writeTimeout))
-				}
-				conn.Write(*data)
-				protocol.PutData(data)
-			}()
-		} else {
+	data := res.EncodeSlicePointer()
+	if s.AsyncWrite {
+		go func() {
 			if s.writeTimeout != 0 {
 				conn.SetWriteDeadline(time.Now().Add(s.writeTimeout))
 			}
 			conn.Write(*data)
 			protocol.PutData(data)
+		}()
+	} else {
+		if s.writeTimeout != 0 {
+			conn.SetWriteDeadline(time.Now().Add(s.writeTimeout))
 		}
-		s.Plugins.DoPostWriteResponse(ctx, req, res, err)
-	}()
+		conn.Write(*data)
+		protocol.PutData(data)
+	}
+	s.Plugins.DoPostWriteResponse(ctx, req, res, err)
 }
 
 func (s *Server) serveConn(conn net.Conn) {
